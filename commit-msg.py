d63cef435232434320fff9367feb24456618db69
Ricardo M <ricardo.martinez@dynatrace.com>
2018-10-02 23:17:18 +0200

    bugfix/APM-123456 test commit

--- commit-msg.py
+++ commit-msg.py
@@ -1,21 +1,37 @@
 #!/usr/bin/env python3
 import sys
+import re
 import platform
 import subprocess
 
 clear_screen_command = 'cls' if platform.system() == 'Windows' else 'clear'
-subprocess.call(clear_screen_command)
+# subprocess.call(clear_screen_command)
 
-# Checking for not staged files, if there's files pending, exit the script
-git_command = ['git', '--no-pager', 'status', '-s']
-execute = subprocess.run(git_command, stdout=subprocess.PIPE)
-modified_files = execute.stdout \
-                        .decode('utf-8') \
-                        .split('\n')
-modified_files = [ file[3:] for file in modified_files if file ]
+# Receive the filename with the temporary git commit msg.
+msg_file = sys.argv[1]
 
-print(modified_files)
+msg_filehandle = open(msg_file)
+with msg_filehandle as f:
+    commit_msg_parts = f.readlines()
+    commit_msg = ''.join(commit_msg_parts)
 
-if len(modified_files):
-    print('Files have been found in the work area, please stash or commit them first and run this command again.\n')
+if not commit_msg:
     sys.exit(1)
+else:
+    execute = subprocess.run(['git', 'branch'], stdout=subprocess.PIPE)
+    branch_list = execute.stdout \
+                         .decode('utf-8') \
+
+    # Extract issue part from active branch for branch list
+    #   * bugfix/APM-123456-test-branch-for-commit-msg
+    #     ^^^^^^^^^^^^^^^^^
+    #     master
+    regex = re.compile(r'(?<=\*\s).+APM-[0-9]*', re.MULTILINE)
+    issue_id = ''.join(re.findall(regex, branch_list))
+
+    msg_filehandle = open(msg_file, 'w')
+    with msg_filehandle as f:
+        complete_commit_msg = '{} {}'.format(issue_id, commit_msg).strip()
+        f.write(complete_commit_msg)
+
+sys.exit(0)
